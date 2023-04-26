ede45838f494646526ecbdd98f899dd131af9b78
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-26 21:15:15 +0500

    variable create
    variable and console result check

---
+++ script.js
@@ -0,0 +1,33 @@
+// var nam=5;
+// var c=10;
+// var j="33.51";
+// j=Number(j);
+// console.log("result",j,typeof(j));
+// var a=100;
+// var name1="kashif";
+// var num2="2.452";
+// // console.log("Result show =",name1 ,typeof(name1));
+// num2=Number(num2);
+
+// console.log("result",num2, typeof(num2));
+
+// console.log("Result show =",nam,typeof(nam));
+// console.log("Result show =",j,typeof(j));
+
+
+// var kashif="4.25";
+// kashif=parseInt(kashif);
+// console.log("convert",kashif,typeof(kashif));
+// var gcuf="50.52";
+// // gcuf=parseInt(gcuf);
+// gcuf=Number(gcuf);
+// console.log(gcuf,typeof(gcuf));
+// var gcuf=030698.10055;
+// gcuf=Number(gcuf);
+// console.log(gcuf,typeof(gcuf));
+
+// var gcuf="kashif";
+// console.log("muhmmad"+ gcuf);
+var gc=20;
+console.log("kashif"+gc+" gc university");
+
